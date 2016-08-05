c2f21e315e078a35df9589c88a079d97d1e3ec2b
Thanooshan <gtshan10@gmail.com>
2016-08-05 17:17:44 +0530

    Update task1.cu

--- task1.cu
+++ task1.cu
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define N 4
- 
-int main()
-{
-   int i, j =0;
+
+__global__ void MatAdd(int A[][N], int B[][N], int C[][N]){
+           int i = threadIdx.x;
+           int j = threadIdx.y;
+
+           C[i][j] = A[i][j] + B[i][j];
+}
+
+
+int main(){
 
   int A[N][N] =
     {
@@ -30,21 +37,43 @@ int main()
       {0, 0, 0, 0}
    };
 
- 
-     for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
-            C[i][j] = A[i][j] + B[i][j];
-        }
-    }
- 
-   printf("Sum of entered matrices:-\n");
- 
+  int (*d_A)[N], (*d_B)[N], (*d_C)[N];
+
+  //Allocate memories for device copies of A,B,C
+  
+  cudaMalloc((void**)&d_A, (N*N)*sizeof(int));
+  cudaMalloc((void**)&d_B, (N*N)*sizeof(int));
+  cudaMalloc((void**)&d_C, (N*N)*sizeof(int));
+  
+  //Allocate space for host copies of A,B,C and setup input values
+
+  cudaMemcpy(d_A, A, (N*N)*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, B, (N*N)*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_C, C, (N*N)*sizeof(int), cudaMemcpyHostToDevice);
+
+  //Launch MatAdd() kernel on GPU
+  int numBlocks = 1;
+  dim3 threadsPerBlock(N,N);
+  MatAdd<<<numBlocks,threadsPerBlock>>>(d_A,d_B,d_C);
+  
+  //copy results back to host
+
+  cudaMemcpy(C, d_C, (N*N)*sizeof(int), cudaMemcpyDeviceToHost);
+
+  int i, j; printf("C = \n");
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
             printf("%d ", C[i][j]);
         }
         printf("\n");
     }
- 
-   return 0;
+
+  //cleanup	
+  cudaFree(d_A); 
+  cudaFree(d_B); 
+  cudaFree(d_C);
+
+  printf("\n");
+
+  return 0;
 }
